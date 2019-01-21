a656d7e822331deb174711dd66f6b8bee47759a0
shubhi31 <mittal.sh@husky.neu.edu>
2019-01-21 16:58:00 -0500

    formatting done

--- homework2/compile.ml
+++ homework2/compile.ml
@@ -3,19 +3,13 @@ open Sexp
 
 (* Abstract syntax of (a small subset of) x86 assembly instructions *)
 
-type reg =
-  | EAX
-  | ESP
+type reg = EAX | ESP
 
-type arg =
-  | Const of int
-  | Reg of reg
-  | RegOffset of int * reg (* int is # words of offset *)
+type arg = Const of int | Reg of reg | RegOffset of int * reg
 
-type instruction =
-  | IMov of arg * arg
-  | IAdd of arg * arg
-  | IRet
+(* int is # words of offset *)
+
+type instruction = IMov of arg * arg | IAdd of arg * arg | IRet
 
 (* Concrete syntax of the Adder language:
 
@@ -31,12 +25,9 @@ type instruction =
 
  *)
 
-
 (* Abstract syntax of the Adder language *)
 
-type prim1 =
-  | Add1
-  | Sub1
+type prim1 = Add1 | Sub1
 
 type 'a expr =
   | Number of int * 'a
@@ -47,57 +38,43 @@ type 'a expr =
 (* Function to convert from unknown s-expressions to Adder exprs
    Throws a SyntaxError message if there's a problem
  *)
-exception SyntaxError of string                  
+
+exception SyntaxError of string
+
 let rec expr_of_sexp (s : pos sexp) : pos expr =
-  (* Returns true if there are no duplicate bindings in the let expression *)
-  (*let rec is_valid? (bs : (string * pos expr) list) : bool =
-    (match bs with
-    | [] -> true
-    | ((v, _)::rest) ->
-        (not (List.mem_assoc v rest)) && (is_valid? rest))
-   in*)
-   match s with
-    | Sym(sym, p) -> Id(sym, p)
-    | Int(i, p) -> Number(i, p)
-    | Nest(lsxp, p) ->
-        (match lsxp with
-           | (Sym("add1", pa)::ex::[]) ->
-                Prim1(Add1, (expr_of_sexp ex), pa)
-           | (Sym("sub1", ps)::ex::[]) ->
-                Prim1(Sub1, (expr_of_sexp ex), ps)
-           | (Sym("let", p1)::Nest(b, p2)::body::[]) ->
-             (* Add a check for empty set of bindings here *)
-             (*let bindings = (expr_of_sexpr_bindings b p1) in *)
-             (* if (is_valid? bindings) then *)
-               Let((expr_of_sexpr_bindings b p1), (expr_of_sexp body), p1)
-            (* else
-               raise (SyntaxError (sprintf "Duplicate bindings found in let expression"))*)
-           | _ ->
-              raise (SyntaxError (sprintf "Invalid nest sexp at pos %s"
-                                    (pos_to_string (sexp_info s) true)))
-        )
-        
-     |_ ->
-       raise (SyntaxError (sprintf "Invalid sexp at pos %s"
-                             (pos_to_string (sexp_info s) true)))
+  match s with
+  | Sym (sym, p) -> Id (sym, p)
+  | Int (i, p) -> Number (i, p)
+  | Nest (lsxp, p) -> (
+    match lsxp with
+    | [Sym ("add1", pa); ex] -> Prim1 (Add1, expr_of_sexp ex, pa)
+    | [Sym ("sub1", ps); ex] -> Prim1 (Sub1, expr_of_sexp ex, ps)
+    | [Sym ("let", p1); Nest (b, p2); body] ->
+        Let (expr_of_sexpr_bindings b p1, expr_of_sexp body, p1)
+    | _ ->
+        raise
+          (SyntaxError
+             (sprintf "Invalid nest sexp at pos %s"
+                (pos_to_string (sexp_info s) true))) )
+  | _ ->
+      raise
+        (SyntaxError
+           (sprintf "Invalid sexp at pos %s" (pos_to_string (sexp_info s) true)))
 
 (* Takes a list of nests representing let bindings and converts then into (string * expr) list *)
-(* expr_of_sexpr_bindings :
-      [ Nest([Sym("x", p11); Int(1, p12)], p1) ;
-       Nest([Sym("y", p21); Int(2, p22)], p2) ...] -> [("x", Number(1)) ; ("y", Number(2))]
- *)
-and expr_of_sexpr_bindings (b : pos sexp list) (p : pos) : (string * pos expr) list =
-    match b with
-      | [] -> []
-      | (n::ns) ->
-       (match n with
-        | Nest(Sym(s, sp)::ex::[], p2) ->
-           [(s, (expr_of_sexp ex))] @ (expr_of_sexpr_bindings ns p)
-        | _ ->
-           raise (SyntaxError (sprintf "Wrong syntax for let bindings at %s"
-                                 (pos_to_string p true))))   
-
-
+and expr_of_sexpr_bindings (b : pos sexp list) (p : pos) :
+    (string * pos expr) list =
+  match b with
+  | [] -> []
+  | n :: ns -> (
+    match n with
+    | Nest ([Sym (s, sp); ex], p2) ->
+        [(s, expr_of_sexp ex)] @ expr_of_sexpr_bindings ns p
+    | _ ->
+        raise
+          (SyntaxError
+             (sprintf "Wrong syntax for let bindings at %s"
+                (pos_to_string p true))) )
 
 (* Functions that implement the compiler *)
 
@@ -106,28 +83,27 @@ and expr_of_sexpr_bindings (b : pos sexp list) (p : pos) : (string * pos expr) l
    for you. *)
 let reg_to_asm_string (r : reg) : string =
   (* COMPLETE THIS FUNCTION *)
-  match r with
-  | EAX -> sprintf "eax"
-  | ESP -> sprintf "esp"
+  match r with EAX -> sprintf "eax" | ESP -> sprintf "esp"
 
 let arg_to_asm_string (a : arg) : string =
   match a with
-  | Const(n) -> sprintf "%d" n
-  | Reg(reg) ->
-     (reg_to_asm_string reg)
-  | RegOffset(offset, reg) ->
-     sprintf "[%s - %d]" (reg_to_asm_string reg) (offset*4)
+  | Const n -> sprintf "%d" n
+  | Reg reg -> reg_to_asm_string reg
+  | RegOffset (offset, reg) ->
+      sprintf "[%s - %d]" (reg_to_asm_string reg) (offset * 4)
 
 let instruction_to_asm_string (i : instruction) : string =
   match i with
-  | IMov(dest, value) ->
-     sprintf "\tmov %s, %s" (arg_to_asm_string dest) (arg_to_asm_string value)
+  | IMov (dest, value) ->
+      sprintf "\tmov %s, %s" (arg_to_asm_string dest) (arg_to_asm_string value)
   | IRet -> sprintf "\tret"
-  | IAdd(dest, value) ->
-     sprintf "\tadd %s, %s" (arg_to_asm_string dest) (arg_to_asm_string value)
+  | IAdd (dest, value) ->
+      sprintf "\tadd %s, %s" (arg_to_asm_string dest) (arg_to_asm_string value)
 
 let to_asm_string (is : instruction list) : string =
-  List.fold_left (fun s i -> sprintf "%s\n%s" s (instruction_to_asm_string i)) "" is
+  List.fold_left
+    (fun s i -> sprintf "%s\n%s" s (instruction_to_asm_string i))
+    "" is
 
 (* A helper function for looking up a name in a "dictionary" and 
    returning the associated value if possible.  This is definitely
@@ -135,75 +111,67 @@ let to_asm_string (is : instruction list) : string =
 let rec find (ls : (string * 'a) list) (x : string) : 'a option =
   match ls with
   | [] -> None
-  | (y, v)::rest ->
-     if y = x then Some(v) else find rest x
+  | (y, v) :: rest -> if y = x then Some v else find rest x
 
 (* The exception to be thrown when some sort of problem is found with names *)
 exception BindingError of string
+
 (* The actual compilation process.  The `compile` function is the primary function,
    and uses `compile_env` as its helper.  In a more idiomatic OCaml program, this
    helper would likely be a local definition within `compile`, but separating it out
    makes it easier for you to test it. *)
-let rec compile_env
-          (p : pos expr)         (* the program, currently annotated with source location info *)
-          (stack_index : int)    (* the next available stack index *)
-          (env : (string * int) list) (* the current binding environment of names to stack slots *)
-        : instruction list =     (* the instructions that would execute this program *)
+let rec compile_env (p : pos expr)
+    (* the program, currently annotated with source location info *)
+    (stack_index : int)
+    (* the next available stack index *)
+    (env : (string * int) list) : instruction list =
+  (* the current binding environment of names to stack slots *)
+  (* the instructions that would execute this program *)
   match p with
-  | Number(n, _) ->
-     [
-       IMov(Reg(EAX), Const(n))
-     ]
-  | Id(v, _) ->
-     (let stack_slot = (find env v) in
+  | Number (n, _) -> [IMov (Reg EAX, Const n)]
+  | Id (v, _) -> (
+      let stack_slot = find env v in
       match stack_slot with
       | None -> raise (BindingError (sprintf "Unbound identifier %s" v))
-      | Some(a) ->
-        [IMov(Reg(EAX), RegOffset(a, ESP))])
-  | Prim1(Add1, e, _) ->
-    (compile_env e stack_index env) @
-      [
-        IAdd(Reg(EAX), Const(1))
-      ]
-  | Prim1(Sub1, e, _) ->
-     (compile_env e stack_index env) @
-       [
-         IAdd(Reg(EAX), Const(-1))
-       ]
-  |Let(bindings, body, _) ->
-    (* compile_bindings returns a list of instructions for named-expressions and
+      | Some a -> [IMov (Reg EAX, RegOffset (a, ESP))] )
+  | Prim1 (Add1, e, _) ->
+      compile_env e stack_index env @ [IAdd (Reg EAX, Const 1)]
+  | Prim1 (Sub1, e, _) ->
+      compile_env e stack_index env @ [IAdd (Reg EAX, Const (-1))]
+  | Let (bindings, body, _) ->
+      (* compile_bindings returns a list of instructions for named-expressions and
        updates the environment with bindings *)
-    let (is, new_env) = (compile_bindings bindings stack_index env) in
-    is @ (compile_env body (stack_index + 1) new_env)
+      let is, new_env = compile_bindings bindings stack_index env in
+      is @ compile_env body (stack_index + 1) new_env
 
-and compile_bindings
-(bs : (string * pos expr) list) (* list of named expressions in let expression *)
-(si : int) (* current stack index *)
-(env : (string * int) list) (* environment in which named-expressions are compiled *)
-     : (instruction list * (string * int) list) (* list of intructions, new environment *)
-  = 
+and compile_bindings (bs : (string * pos expr) list)
+    (* list of named expressions in let expression *)
+    (si : int)  (* current stack index *)
+               (env : (string * int) list) :
+    instruction list * (string * int) list =
+  (* environment in which named-expressions are compiled *)
+  (* list of intructions, new environment *)
   match bs with
-  | [] ->
-     ([], env)
-  | ((v, exp)::rs) ->
-     let iexp = (compile_env exp si env) @ [IMov(RegOffset(si, ESP), Reg(EAX))] in
-     let irest, env_rest = (compile_bindings rs (si + 1) ([(v, si)] @ env)) in
-     let iall = iexp @ irest in
-     (*let local_env  = [(v, si)] @ env_rest in*)
-     (iall, env_rest)
-     
-     
+  | [] -> ([], env)
+  | (v, exp) :: rs ->
+      let iexp =
+        compile_env exp si env @ [IMov (RegOffset (si, ESP), Reg EAX)]
+      in
+      let irest, env_rest = compile_bindings rs (si + 1) ([(v, si)] @ env) in
+      let iall = iexp @ irest in
+      (*let local_env  = [(v, si)] @ env_rest in*)
+      (iall, env_rest)
 
-let compile (p : pos expr) : instruction list =
-  compile_env p 1 [] (* Start at the first stack slot, with an empty environment *)
+let compile (p : pos expr) : instruction list = compile_env p 1 []
+
+(* Start at the first stack slot, with an empty environment *)
 
 (* The entry point for the compiler: a function that takes a expr and
    creates an assembly-program string representing the compiled version *)
 let compile_to_string (prog : pos expr) : string =
-  let prelude = "
-section .text
-global our_code_starts_here
-our_code_starts_here:" in
-  let asm_string = (to_asm_string ((compile prog) @ [IRet])) in
+  let prelude =
+    "\nsection .text\nglobal our_code_starts_here\nour_code_starts_here:"
+  in
+  let asm_string = to_asm_string (compile prog @ [IRet]) in
   let asm_program = sprintf "%s%s\n" prelude asm_string in
   asm_program
--- homework2/test.ml
+++ homework2/test.ml
@@ -4,64 +4,62 @@ open Printf
 open OUnit2
 
 let t (name : string) (program : string) (expected : string) : OUnit2.test =
-  name>::test_run program name expected;;
-let te (name : string) (program : string) (expected_err : string) : OUnit2.test =
-  name>::test_err program name expected_err;;
+  name >:: test_run program name expected
+
+let te (name : string) (program : string) (expected_err : string) : OUnit2.test
+    =
+  name >:: test_err program name expected_err
 
 let suite : OUnit2.test =
-"suite">:::
- [
-   t "forty_one" "41" "41";
-   t "add_1_const" "(add1 2)" "3";
-   t "sub_1_const" "(sub1 2)" "1";
-   t "let_0" "(let ((x 10)) x)" "10";
-   t "let_1" "(let ((x 2)) (add1 x))" "3";
-   t "let_2" "(let ((x (add1 2))) (add1 x))" "4";
-   t "let_3" "(let ((x 3) (y 4)) (add1 x))" "4";
-   t "let_4" "(let ((x 3) (y 4)) (add1 y))" "5";
-   t "let_5" "(let ((x 3) (y 4) (z 10) (w 8)) w)" "8";
-   t "let_6" "(let ((x 2)) (let ((y (add1 x))) (add1 y)))" "4";
-   t "let_7" "(let ((x 2)) (let ((x (add1 x))) x))" "3"; 
-   
-   (* lexical scope is maintained *)
-   t "let_8" "(let ( (x 1) (y 2) )
-                (let ( (z (add1 y)) )
-                  (let ( (y (add1 z) ) ) x ))) " "1"; 
-   
-   t "let_9" "(let ( (x 1) (y 2) )
-                (let ( (z (add1 y)) )
-                  (let ( (x (add1 z) ) ) x ))) " "4"; 
-   
-   t "let_10" "(let ( (x 1) (y 2) )
-                (let ( (z (add1 y)) )
-                  (let ( (y (add1 z) ) ) z ))) " "3";
+  "suite"
+  >::: [ t "forty_one" "41" "41"
+       ; t "add_1_const" "(add1 2)" "3"
+       ; t "sub_1_const" "(sub1 2)" "1"
+       ; t "let_0" "(let ((x 10)) x)" "10"
+       ; t "let_1" "(let ((x 2)) (add1 x))" "3"
+       ; t "let_2" "(let ((x (add1 2))) (add1 x))" "4"
+       ; t "let_3" "(let ((x 3) (y 4)) (add1 x))" "4"
+       ; t "let_4" "(let ((x 3) (y 4)) (add1 y))" "5"
+       ; t "let_5" "(let ((x 3) (y 4) (z 10) (w 8)) w)" "8"
+       ; t "let_6" "(let ((x 2)) (let ((y (add1 x))) (add1 y)))" "4"
+       ; t "let_7" "(let ((x 2)) (let ((x (add1 x))) x))" "3"
+       ; (* lexical scope tests *)
+         t "let_8"
+           "(let ( (x 1) (y 2) )\n\
+           \                (let ( (z (add1 y)) )\n\
+           \                  (let ( (y (add1 z) ) ) x ))) "
+           "1"
+       ; t "let_9"
+           "(let ( (x 1) (y 2) )\n\
+           \                (let ( (z (add1 y)) )\n\
+           \                  (let ( (x (add1 z) ) ) x ))) "
+           "4"
+       ; t "let_10"
+           "(let ( (x 1) (y 2) )\n\
+           \                (let ( (z (add1 y)) )\n\
+           \                  (let ( (y (add1 z) ) ) z ))) "
+           "3"
+       ; t "let_11" "(let ((x (let ((y (add1 2))) (sub1 y)))) x)" "2"
+       ; t "let_12" "(let ((x 5) (y x)) y)" "5"
+       ; t "let_13" "(let ((x 5) (y (sub1 x))) (sub1 y))" "3"
+       ; t "let_14" "(let ((x 5) (y (sub1 x)) (z (sub1 y))) (sub1 z))" "2"
+       ; t "let_15" "(let ((x 5) (y 6) (z y)) z)" "6"
+       ; t "let_16" "(let ((x (let ((x 5)) (sub1 x)))) x)" "4"
+       ; (* scary nested let named expressions *)
+         t "let_17"
+           "(let ((z (let ( (x 1) (y 2) )\n\
+           \                           (let ( (z (add1 y)) )\n\
+           \                              (let ( (z (add1 z) ) ) z ))))) z) "
+           "4"
+       ; t "let_18"
+           "(let ((z 1))\n\
+           \        (let ( (z (let ((y 2)) \n\
+           \                     (let ((z (add1 y)))\n\
+           \                        (let ((z (add1 z))) z ))))) z))"
+           "4"
+       ; te "x_err" "x" "Unbound identifier"
+       ; te "let_err_1" "(let ((x (add1 x))) (add1 x))" "Unbound identifier"
+       ; te "let_err_2" "(let ((x (add1 y))) (add1 x))" "Unbound identifier"
+       ; te "let_err_3" "(let ((x 2) (y 3) (x 5)) x)" "Duplication binding" ]
 
-   t "let_11" "(let ((x (let ((y (add1 2))) (sub1 y)))) x)" "2";
-   t "let_12" "(let ((x 5) (y x)) y)" "5";
-   t "let_13" "(let ((x 5) (y (sub1 x))) (sub1 y))" "3";
-   t "let_14" "(let ((x 5) (y (sub1 x)) (z (sub1 y))) (sub1 z))" "2";
-   t "let_15" "(let ((x 5) (y 6) (z y)) z)" "6";
-   t "let_16" "(let ((x (let ((x 5)) (sub1 x)))) x)" "4";
-
-   (* nested let named expressions *)
-   t "let_17" "(let ((z (let ( (x 1) (y 2) )
-                           (let ( (z (add1 y)) )
-                              (let ( (z (add1 z) ) ) z ))))) z) " "4";
-
-  t "let_18" 
-      "(let ((z 1))
-        (let ( (z (let ((y 2)) 
-                     (let ((z (add1 y)))
-                        (let ((z (add1 z))) z ))))) z))" "4";
-
-   te "x_err" "x" "Unbound identifier";
-   te "let_err_1" "(let ((x (add1 x))) (add1 x))" "Unbound identifier";
-   te "let_err_4" "(let ((x (add1 y))) (add1 x))" "Unbound identifier";
-  
-    ]
-;;
-
-
-let () =
-  run_test_tt_main suite
-;;
+let () = run_test_tt_main suite
