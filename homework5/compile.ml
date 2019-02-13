faa1232db4533a804d23440fae6ed24b5d08b3eb
shubhi31 <mittal.sh@husky.neu.edu>
2019-02-13 17:54:15 -0500

    Added is_well_formed function

--- homework5/compile.ml
+++ homework5/compile.ml
@@ -66,6 +66,13 @@ let rec find_decl (ds : 'a decl list) (name : string) : 'a decl option =
     | (DFun(fname, _, _, _) as d)::ds_rest ->
       if name = fname then Some(d) else find_decl ds_rest name
 
+(* helper that finds the binding in the list of bindings so called local env *)
+let rec find_bind (bs : 'a bind list) (name : string) : 'a bind option =
+  match bs with
+    | [] -> None
+    | ((bname, _, _) as b)::bs_rest ->
+     if name = bname then Some(b) else find_bind bs_rest name
+
 let rec find_one (l : 'a list) (elt : 'a) : bool =
   match l with
     | [] -> false
@@ -152,26 +159,101 @@ let anf (p : tag program) : unit aprogram =
 
 
 let is_well_formed (p : sourcespan program) : (sourcespan program) fallible =
-  let rec wf_E (e : 'a expr) : exn list =
-    []
+  let giant_let (bindings : 'a bind list) (body : 'a expr) (pos : sourcespan) : 'a expr =
+    List.fold_right
+    (fun (b : 'a bind) (body : 'a expr) ->
+      ELet([b], body, pos)
+    )
+    bindings
+    body
+    in
+  let rec wf_E (e : 'a expr) (global_env : 'a decl list) (local_env : 'a bind list) : exn list =
+      match e with
+       | ENumber(n, pos) ->
+        (* check for overflow and underflow at compile time for large enough values *)
+        []
+       | EBool(b, pos) -> []
+       | EId(id, pos) ->
+          (match (find_bind local_env id) with
+            | None -> []
+            | _ -> [UnboundId(id, pos)]
+          )
+       | EIf(cond, thn, els, pos) ->
+             let cond_err = (wf_E cond global_env local_env) in
+             let then_err = (wf_E thn global_env local_env) in
+             let else_err = (wf_E els global_env local_env) in
+             cond_err @ then_err @ else_err
+      | EPrim1(prim1, e1, pos) ->
+        (wf_E e1 global_env local_env)
+      | EPrim2(prim2, e1, e2, pos) ->
+        let e1_err = (wf_E e1 global_env local_env) in
+        let e2_err = (wf_E e2 global_env local_env) in
+        e1_err @ e2_err
+      | ELet(bindings, body, pos) ->
+        let big_let = giant_let bindings body pos in
+        (match big_let with
+          | ELet([(v2, e2, p2) as b], body, pos) ->
+              let dup_bind_err = (match (find_bind local_env v2) with
+                                  | None -> []
+                                  | Some((v1, e1, p1)) -> [DuplicateId(v1, p2, p1)])
+              in
+              let bind_expr_err =  (wf_E e2 global_env local_env) in
+              let body_expr_err =  (wf_E body global_env (local_env @ [b])) in
+              dup_bind_err @ bind_expr_err @ body_expr_err
+          |_ ->
+            let err_msg = "Expected a let with single binding but found " ^ (ast_of_expr e) in
+            [InternalCompilerError(err_msg)])
+      | EApp(funname, args, pos) ->
+        let f_decl = (find_decl global_env funname) in
+        let fun_errs = (match f_decl with
+                          | None ->
+                              [UnboundFun(funname, pos)]
+                          | Some(DFun(fname, fargs, fbody, fp)) ->
+                                let actual_arity = (List.length args) in
+                                let intended_arity = (List.length fargs) in
+                                if (actual_arity != intended_arity)
+                                then [Arity(intended_arity,actual_arity, pos)]
+                                else [])
+       in
+       let arg_errs = List.fold_left
+       (fun (err : exn list) (arg : 'a expr) ->
+          let arg_err = (wf_E arg global_env local_env) in
+          err @ arg_err
+       )
+       []
+       args
+       in
+       fun_errs @ arg_errs
   and wf_D (d : 'a decl) (env : 'a decl list) : exn list =
      match d with
      | DFun(funname, args, body, pos_u) ->
-        let fun_name_err = match (find_decl env funname) with
+        let fun_name_err = (match (find_decl env funname) with
          | Some(DFun(_, _, _, pos_d)) -> [DuplicateFun(funname, pos_u, pos_d)]
-         | None -> []
+         | None -> [])
         in
-        let bind_err = match (find_dup args) with
+        let bind_err = (match (find_dup args) with
         | Some((b, pos_b)) -> [DuplicateId(b, pos_b, pos_u)]
-        | None -> []
+        | None -> [])
         in
-        let body_err = wf_E body
+        let body_err = (wf_E body env [])
         in
         fun_name_err @ bind_err @ body_err
   in
   match p with
   | Program(decls, body, _) ->
-     Error([NotYetImplemented "Implement well-formedness checking for programs"])
+     (* global environment is the list of declarations *)
+     let (decl_exns, global_env) = List.fold_left
+       (fun ((errs : exn list), (env : 'a decl list)) (d : 'a decl) ->
+          (* Note even adding the wrong decl to env does not impact evaluation since we always find the first
+             defined decl in case of the duplicate decls
+           *)
+          (errs @ (wf_D d env), env @ [d])
+       )
+       ([], [])
+       decls
+     in
+     let body_err = (wf_E body global_env []) in
+     (p , decl_exns @ body_err)
 ;;
 
 
